a738e686decce835d738b177ae1cd168babbba16
Monty Anderson <work@montyanderson.net>
2020-10-28 15:05:29 +0000

    use https for ipstack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -115,7 +115,7 @@ export default {
 		Keys
 	},
 	async created() {
-		const { data } = await axios.get('http://api.ipstack.com/check?access_key=fd027f23eda5344acc1bfabce62b0436');
+		const { data } = await axios.get('https://api.ipstack.com/check?access_key=fd027f23eda5344acc1bfabce62b0436');
 
 		const satellites = {
 			'us-central-1': [41.2619, -95.8608],
