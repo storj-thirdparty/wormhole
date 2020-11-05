1dfc0593572420002e84d814171d89f9d2b8f61d
Monty Anderson <work@montyanderson.net>
2020-11-05 16:42:03 +0000

    active-accounts show satellite counts

--- scripts/active-accounts.js
+++ scripts/active-accounts.js
@@ -2,18 +2,28 @@ const parse = require('csv-parse');
 const r = require('rethinkdb');
 
 (async () => {
-	const conn = await require('../lib/connection');
+        const conn = await require('../lib/connection');
 
-	const cursor = await r.table('accounts2')
-		.filter(r.row('userEmail'))
-		.run(conn);
+        const cursor = await r.table('accounts2')
+                .filter(r.row('userEmail'))
+                .run(conn);
 
-	const arr = await cursor.toArray();
+        const arr = await cursor.toArray();
 
-	for(const user of arr) {
-		console.log(user);
-	}
+        for(const user of arr) {
+                console.log(user);
+        }
 
-	console.log('Found', arr.length, 'accounts');
+        console.log('Found', arr.length, 'accounts');
+
+        async function printSatelliteUsed(satellite) {
+                console.log(satellite, arr.filter(el => el.satelliteAddress == satellite).length)
+        }
+
+        printSatelliteUsed('europe-west-1.tardigrade.io');
+        printSatelliteUsed('us-central-1.tardigrade.io');
+        printSatelliteUsed('asia-east-1.tardigrade.io');
+
+        process.exit(0);
 
 })();
